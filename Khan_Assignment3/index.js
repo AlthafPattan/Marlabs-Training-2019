03eda38f689c4b7412eef9fde821a5d129e78fac
Khanif3693 <khanha@oregonstate.edu>
2019-08-19 11:56:35 -0400

    Update index.js

--- Khan_Assignment3/index.js
+++ Khan_Assignment3/index.js
@@ -1,1 +1,22 @@
 
+// Wait for the DOM to be ready
+var data = {};
+$(document).ready(function() {
+  //console.log("Document is loaded");
+
+  let flag = true;
+
+  $("#SignIn_Submit").click(function(event) {
+    // console.log(data);
+    if (data[$("#SignIn_Username").val()] == $("#SignIn_Password").val()) {
+      console.log("SignIn Success");
+      $("#home").html("<h1>You are successfully Logged In</h1>");
+    }
+  });
+
+  $("#SignUp_Submit").click(function(event) {
+    //console.log("I am submitted");
+    data[$("#Signup_Username").val()] = $("#SignUp_Password").val();
+    // console.log(data);
+  });
+});
