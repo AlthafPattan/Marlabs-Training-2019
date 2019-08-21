4502f0c58da8037fc8e16d0b8809e97104c0bb64
Chopardissmo <gel@usc.edu>
2019-08-21 12:08:13 -0400

    finish

---
+++ userClass.js
@@ -0,0 +1,10 @@
+class user {
+    constructor (firstName, lastName, userId, email, password, gender, ) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userId = userId;
+        this.email = email;
+        this.password = password;
+        this.gender = gender;
+    }
+}
