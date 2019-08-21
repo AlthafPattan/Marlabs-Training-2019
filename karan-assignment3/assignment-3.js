c5e62a49a1b51e612023d4f6d38a36971406a001
KARAN RUMALWALA <karanrumalwala9@gmail.com>
2019-08-21 12:46:08 -0400

    Add files via upload
    Final Project

--- karan-assignment3/assignment-3.js
+++ karan-assignment3/assignment-3.js
@@ -1,56 +1,18 @@
 
-/////////////////////////valid details///////////////////////////////////////////////////
-// function validDetail(){
-    // var firstName= document.getElementById('inputFirstname');
-    // var lastName= document.getElementById('inputLastname');
-    // var uniqueID= document.getElementById('userid');
-    // var address=document.getElementById('inputAddress');
-    // var city= document.getElementById('inputCity');
-    // var state= document.getElementById('inputState');
-    // var agree= document.getElementById('gridCheck');
-        // if(firstName.value=='')
-        // firstName.setCustomValidity("Please enter FirstName");
-        // else
-        // firstName.setCustomValidity("");
-        // if(lastName.value=='')
-        // lastName.setCustomValidity("Please enter LastName");
-        // else
-        // lastName.setCustomValidity("");
-        // if(uniqueID.value=='') 
-        // uniqueID.setCustomValidity("Please enter User Id");
-        // else
-        // uniqueID.setCustomValidity("");
-        // if(address.value=='')
-        // address.setCustomValidity("Please enter address");
-        // else
-        // address.setCustomValidity("");
-        // if(city.value=='')
-        // city.setCustomValidity("Please enter city");
-        // else
-        // city.setCustomValidity("");
-        // if(state.value=='')
-        // state.setCustomValidity("Please enter state");
-        // else
-        // state.setCustomValidity("");
-        // if(agree.checked == false)
-        // agree.setCustomValidity("please check the box");
-        // else
-        // agree.setCustomValidity("");
-// }
 ///////////////////////////////// passssword/////////////////////////////////////////////
 function validPass(){
-    var password= document.getElementById('passwrd').value;
+    var password1= document.getElementById('passwrd').value;
     var conPass= document.getElementById("inputConPassword4").value;
         var minMaxLength = /^[\s\S]{8,32}$/,
             upper = /[A-Z]/,
             lower = /[a-z]/,
             special = /[ !"#$%&'()*+,\-./:;<=>?@[\\\]^_`{|}~]/;
-        if (minMaxLength.test(password) &&
-            upper.test(password) &&
-            lower.test(password) &&
-            special.test(password)
+        if (minMaxLength.test(password1) &&
+            upper.test(password1) &&
+            lower.test(password1) &&
+            special.test(password1)
         ) {           
-            if (password != conPass) { 
+            if (password1 != conPass) { 
                     document.getElementById("error").innerHTML="Password doesnot match";
                     return false; 
                 } else{
@@ -62,6 +24,7 @@ function validPass(){
         }else{
             document.getElementById("error").innerHTML=" Atleast 1sysmbol, 1UpperCase and 1Lowercase char";
         }
+
 }
 
 function checkValid(){
@@ -77,25 +40,31 @@ var p1=document.getElementById("exampleInputPassword1");
         alert("Please check credentials,try again");
     }
 }
-
-var paswrd1= document.getElementById('passwrd');
-var firstName= document.getElementById('inputFirstname');
-var lastName= document.getElementById('inputLastname');
-var gender= document.getElementById('gridCheck');
-var uniqueID= document.getElementById('userid');
-var address=document.getElementById('inputAddress');
-var city= document.getElementById('inputCity');
-var state= document.getElementById('inputState');
-var email= document.getElementById('inputEmail4');
-
-function storeLocal(){
-   localStorage.setItem("firstname",firstName.value);
-   localStorage.setItem("lastname",lastName.value);
-   localStorage.setItem("email",email.value);
-   localStorage.setItem("uniqueid",uniqueID.value);
-   localStorage.setItem("address",address.value);
-   localStorage.setItem("city",city.value);
-   localStorage.setItem("state",state.value);
-   localStorage.setItem("gender",gender.value);
-   localStorage.setItem("password",paswrd1.value);
+class user{
+    constructor (firstName,lastName,gender,uniqueID,address,city,state,email,password){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.gender=gender;
+        this.uniqueID=uniqueID;
+        this.address=address;
+        this.city=city;
+        this.state=state;
+        this.email=email;
+        this.password=password;
+    }
 }
+function storeLocal(){
+var firstName= document.getElementById('inputFirstname').value;
+var lastName= document.getElementById('inputLastname').value;
+var gender= document.getElementById('gridCheck').value;
+var uniqueID= document.getElementById('userid').value;
+var address=document.getElementById('inputAddress').value;
+var city= document.getElementById('inputCity').value;
+var state= document.getElementById('inputState').value;
+var email= document.getElementById('inputEmail4').value;
+var password= document.getElementById('passwrd').value;
+
+let userData= new user(firstName,lastName,gender,uniqueID,address,city,state,email,password);
+localStorage.setItem(uniqueID,JSON.stringify(userData));
+}
+
